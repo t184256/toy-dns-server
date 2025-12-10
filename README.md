a1affd301c7f649eb6881b2cf1b5a873642371a3
Alexander Sosedkin <monk@unboiled.info>
2025-12-10 19:43:14 +0100

    start with some boilerplate

---
+++ README.md
@@ -0,0 +1,3 @@
+# A toy DNS server written in Rust for learning purposes
+
+Design decisions: async, no threads.
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use toy_dns_server;
+
+fn main() {
+    let result = toy_dns_server::add(3, 3);
+    println!("3 + 3 = {}", result);
+}
